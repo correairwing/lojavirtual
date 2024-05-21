3f4e97ceebc579631c7b1ef5411106ea22cceaf2
Irwing <irwingcorrea95@gmail.com>
2024-05-21 17:17:23 -0300

    feat: crud de categorias

---
+++ backend/src/main/java/com/icorrea/backend/service/CategoriaService.java
@@ -0,0 +1,39 @@
+package com.icorrea.backend.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.icorrea.backend.entity.Categoria;
+import com.icorrea.backend.repository.CategoriaRepository;
+
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+
+    public List<Categoria> buscarTodos() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria inserir(Categoria objeto) {
+        objeto.setDataCriacao(new Date());
+        Categoria objetoNovo = categoriaRepository.saveAndFlush(objeto);
+        return objetoNovo;        
+    }
+
+    public Categoria alterar(Categoria objeto) {
+        objeto.setDataAtualizacao(new Date());
+        return categoriaRepository.saveAndFlush(objeto);
+    }
+
+    public void excluir(Long id){
+        Categoria objeto = categoriaRepository.findById(id).get();
+        categoriaRepository.delete(objeto);
+    }
+}
